9a9c8844a82c08e341d6720a010f36c82328ff35
mung96 <mung5079@gmail.com>
2023-11-22 12:19:31 +0900

    refactor: create()을 Answer생성까지로 변경
    기존: random번호 생성까지
    현재: Answer객체 생성까지

--- src/main/java/baseball/domain/AnswerMachine.java
+++ src/main/java/baseball/domain/AnswerMachine.java
@@ -9,14 +9,20 @@ public class AnswerMachine {
     private static final Integer MIN_POSSIBLE_NUMBER = 1;
     private static final Integer MAX_POSSIBLE_NUMBER = 9;
     private static final Integer ANSWER_SIZE = 3;
-    List<Integer> computer = new ArrayList<>();
 
-    public void createAnswer() {
+    public Answer create() {
+        List<Integer> computer = choiceRandomNumbers();
+        return new AnswerImpl(computer);
+    }
+
+    private List<Integer> choiceRandomNumbers() {
+        List<Integer> computer = new ArrayList<>();
         while (computer.size() < ANSWER_SIZE) {
             int randomNumber = Randoms.pickNumberInRange(MIN_POSSIBLE_NUMBER, MAX_POSSIBLE_NUMBER);
             if (!computer.contains(randomNumber)) {
                 computer.add(randomNumber);
             }
         }
+        return computer;
     }
 }
