28f2cff1965c64afec1043875eff4ff6ccb97459
mung96 <mung5079@gmail.com>
2023-11-22 23:10:30 +0900

    feat:최종 Application 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,21 @@
 package baseball;
 
+import baseball.Controller.Game;
+import baseball.view.InputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Game game = new Game();
+
+        game.noticeStart();
+        game.play();
+
+        while (isRestart()) {
+            game.play();
+        }
+    }
+
+    private static boolean isRestart() {
+        return InputView.askRestart().equals("1");
     }
 }
